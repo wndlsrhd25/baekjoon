fb1ffe8c88f9456a17d8b7155a1e040fc037bfcb
hihijoo <shrkfl4555@gmail.com>
2022-07-14 17:48:58 +0900

    7/14

--- baekjoon/src/baekjoon3_반복문/ch10950_ab3.java
+++ baekjoon/src/baekjoon3_반복문/ch10950_ab3.java
@@ -7,6 +7,15 @@ public class ch10950_ab3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int a = sc.nextInt();
+		
+		for(int i=0; i<a; i++) {
+			for(int j=1; j<3; j++) {
+				int b =sc.nextInt();
+			}
+			
+		}
+		System.out.println(b+c);
 		
 	}
 
