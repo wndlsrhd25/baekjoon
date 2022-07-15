1212a3780cec5bbb090c5cdf726d8c8cadd3bc62
hihijoo <shrkfl4555@gmail.com>
2022-07-15 16:05:01 +0900

    7/15

--- baekjoon/src/baekjoon3_반복문/ch11022_ab8.java
+++ baekjoon/src/baekjoon3_반복문/ch11022_ab8.java
@@ -1,14 +1,12 @@
 package baekjoon3_반복문;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class ch11022_ab8 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
 
@@ -18,25 +16,6 @@ public class ch11022_ab8 {
 			int c = sc.nextInt();
 			System.out.println("Case #"+i+": "+b+" + "+c+" = "+(b+c));
 		}
-	}
-
-		
-		
-		
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-	int a = Integer.parseInt(br.readLine());
-	
-	StringTokenizer st;
-	
-	for (int i=0; i<a+1; i++) {
-		st = new StringTokenizer(br.readLine()," ");
-		
-	
-	System.out.println("Case #"+i+": "+(Integer.parseInt(st.nextToken())+"+"+(Integer.parseInt(st.nextToken()))+"=" +(Integer.parseInt(st.nextToken())+(Integer.parseInt(st.nextToken())));
-	
-	}
-
 	}
 
 }
