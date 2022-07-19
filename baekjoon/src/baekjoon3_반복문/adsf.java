39da7d35586a9a8672008f85b6d4f025ea991860
hihijoo <shrkfl4555@gmail.com>
2022-07-19 17:03:49 +0900

    7/19

--- baekjoon/src/baekjoon3_반복문/adsf.java
+++ baekjoon/src/baekjoon3_반복문/adsf.java
@@ -1,41 +1,30 @@
 package baekjoon3_반복문;
 
+
+
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-
-public class ch10951_ab4 {
-
-	public static void main(String[] args) throws IOException {
+ 
+public class adsf {
+	public static void main(String args[]) throws IOException {
 		
-//		Scanner sc = new Scanner(System.in);
-//
-//		//hasNext() 다음에 가져올 값이 잇으면 true, 없으면 false
-//		while (sc.hasNext()) {
-//			int a = sc.nextInt();
-//			int b = sc.nextInt();
-//			System.out.println(a+b);
-//			
-//		}
-//		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		String str;
-		
-		
-		while((str=br.readLine())!=null) {
+ 
+		while( (str=br.readLine()) != null ){
+		    
 			st = new StringTokenizer(str," ");
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			sb.append(a+b).append("\n");
-			
+		
 		}
 		System.out.print(sb);
-		
-		
 	}
-
 }
 
